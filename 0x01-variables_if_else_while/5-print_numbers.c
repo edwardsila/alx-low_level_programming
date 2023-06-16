6853dab868106414c3ad8a986e84856e306649c0
kenyanthugger <edwardsila000@gmail.com>
2023-06-16 12:44:15 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit number of base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int d = 0;
+
+	while (d <= 10)
+	{
+		putchar(d);
+		d++;
+	}
+
+	return (0);
+}
