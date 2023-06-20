aab3d20da474a83b6ae185e096f58fedc08ca61a
kenyanthugger <edwardsila000@gmail.com>
2023-06-20 13:49:13 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabets 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int n;
+	int c;
+
+	for (n = 0; n <= 10; n++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,5 +5,7 @@ void _putchar(char c);
 
 /* prints alphabet in lowercase */
 void print_alphabet(void);
+/* prints the alphabets in small letters 10 times */
+void print_alphabet_x10(void);
 
 #endif /* MAIN_H */
