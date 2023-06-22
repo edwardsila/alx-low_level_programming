ed030a8066c8b881e3eea50709d9d8cadf25444f
kenyanthugger <edwardsila000@gmail.com>
2023-06-22 14:51:27 -0700

    trinagle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_triangle - prints a triangle
+ *@size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int height;
+	int width;
+	int draw;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (height = 1; height <= size; height++)
+	{
+		for (width = 1; width <= (size - height); width++)
+			_putchar(' ');
+
+		for (draw = 1; draw <= height; draw++)
+			_putchar('#');
+
+	_putchar('\n');
+	}
+}
