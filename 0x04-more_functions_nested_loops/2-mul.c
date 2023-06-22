7578fc14798d80b47e6b3646ea9cfe29a96e14d9
kenyanthugger <edwardsila000@gmail.com>
2023-06-22 12:10:12 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies to integers
+ * @a: integer
+ * @b: integer
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	_putchar('0' + (a * b));
+	_putchar('\n');
+
+	return (0);
+}
