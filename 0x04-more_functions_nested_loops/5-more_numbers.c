ddfd0fdfc2d3d3188c8bc09dc8fab6205b36766f
kenyanthugger <edwardsila000@gmail.com>
2023-06-22 13:15:00 -0700

    more nested

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,6 +10,8 @@ void more_numbers(void)
 
 	for (i = 0; i <= 10; i++)
 		for (n = 0; n <= 14; n++)
-			_putchar('0' + n);
+			if (n > 9)
+				_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
 	_putchar('\n');
 }
