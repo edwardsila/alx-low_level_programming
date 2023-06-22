8ae1a9af68ca4e9a6d591a36f946da6ac1feeaae
kenyanthugger <edwardsila000@gmail.com>
2023-06-22 13:42:24 -0700

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - prints a straight line
+ * @n: integer
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	if (n >= 0)
+		_putchar('_' * n);
+		_putchar('\n');
+	else
+		_putchar('\n');
+
+	return (0);
+}
