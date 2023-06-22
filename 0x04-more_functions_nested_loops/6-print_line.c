d648f0327515d70cfa91cf844bb240f363546e21
kenyanthugger <edwardsila000@gmail.com>
2023-06-22 13:52:28 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,6 @@
 /**
  * print_line - prints a straight line
  * @n: integer
- * Return: 0
  */
 
 void print_line(int n)
@@ -12,11 +11,10 @@ void print_line(int n)
 	if (n >= 0)
 	{
 		for (i = 0; i < n; i++)
-			_Putchar('_');
+			_putchar('_');
 		_putchar('\n');
 	}
 	else
 		_putchar('\n');
 
-	return (0);
 }
