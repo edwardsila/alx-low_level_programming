bc85472566596859edbc7ee30fddddcad3ce7680
kenyanthugger <edwardsila000@gmail.com>
2023-06-27 11:57:59 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns the lenght of a string
+ * @s: pointer to a charachter
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s != '\0')
+	{
+		++length;
+		++s;
+	}
+	return (length);
+}
