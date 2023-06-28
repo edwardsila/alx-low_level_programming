0026a539ebb6bdda721f1d970e3494b80376bfa3
kenyanthugger <edwardsila000@gmail.com>
2023-06-27 23:50:35 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * _strlen - calculates length of string
+ * @s: pointer to char
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		++length;
+		++s;
+	}
+
+	return (length);
+}
+
+/**
+ * puts_half - prints half of a string
+ * @str: pointer to a string
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int length = _strlen(str);
+	int start_index;
+
+	if (length % 2 == 0)
+		start_index = length / 2;
+	else
+		start_index = (length - 1) / 2;
+
+	int i;
+
+	for (i = start_index; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\0');
+}
