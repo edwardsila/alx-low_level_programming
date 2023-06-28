35c16e77233d9b06481ad19cc500ea1cb65ab359
kenyanthugger <edwardsila000@gmail.com>
2023-06-28 09:50:28 -0700

    atoi

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,6 +12,6 @@ void rev_string(char *s);
 void puts2(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoic(char *s);
+int _atoi(char *s);
 
 #endif /* MAIN_H */
