f3123fe783d681ef73754995d372f9d50997f45f
kenyanthugger <edwardsila000@gmail.com>
2023-06-27 23:52:04 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -27,14 +27,13 @@ int _strlen(char *s)
 void puts_half(char *str)
 {
 	int length = _strlen(str);
-	int start_index;
+	int start_index, i;
 
 	if (length % 2 == 0)
 		start_index = length / 2;
 	else
 		start_index = (length - 1) / 2;
 
-	int i;
 
 	for (i = start_index; i < length; i++)
 	{
