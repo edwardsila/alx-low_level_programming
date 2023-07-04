e80274e64e6528eec429e53e02ef5b11eae43604
kenyanthugger <edwardsila000@gmail.com>
2023-07-04 10:55:24 -0700

    correction of 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,9 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-			return (s);
-		i++;
-	}
+	while (*s != '\0' && *s != c)
+		s++;
 
-	return (0);
+	return ((*s == c) ? (char *) s : NULL);
+
 }
