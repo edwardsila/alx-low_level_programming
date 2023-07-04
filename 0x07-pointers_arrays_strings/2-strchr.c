7ea3dfb574f6b2af518806ec85c68d5738b75441
kenyanthugger <edwardsila000@gmail.com>
2023-07-04 02:23:02 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to string
+ * @c: char
+ * Return: s and NULL if otherwise
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s);
+		i++;
+	}
+
+	return (NULL);
+}
