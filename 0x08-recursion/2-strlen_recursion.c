7425965ab6773d410d00a3c50bc92e2c6664adea
kenyanthugger <edwardsila000@gmail.com>
2023-07-05 11:30:08 -0700

    length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - return the length of a string
+ * @s: pointer to a string
+ * Return: length
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	return (1 + _strlen_recursion(s + 1));
+}
