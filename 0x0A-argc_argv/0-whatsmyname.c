6be98366549a564783da738eccc75bfd919fe809
kenyanthugger <edwardsila000@gmail.com>
2023-07-07 11:22:36 -0700

    print my own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: succes (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	if (i < argc)
+	{
+		printf("%s \n", argv[i]);
+	}
+	return (0);
+}
