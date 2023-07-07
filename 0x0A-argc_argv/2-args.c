dd1e84548bd5f6463aee13ef324adc92b25fdf49
kenyanthugger <edwardsila000@gmail.com>
2023-07-07 12:00:40 -0700

    all arg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments it received
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+		argc += 1;
+	}
+
+	return (0);
+}
