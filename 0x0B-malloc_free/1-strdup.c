aee85a07ba455acb09a6106fc9ca5de50f7377b0
kenyanthugger <edwardsila000@gmail.com>
2023-07-14 11:28:05 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,13 +15,18 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	if (*str != '\0')
+	if (str[len] != '\0')
 		len++;
+
 	str1 = (char *)malloc((len + 1) * sizeof(char));
+
+	if (str1 == NULL)
+		return (NULL);
 
 	for (i = 0; i < len; i++)
 	{
 		str1[i] = str[i];
 	}
+	str1[len] = '0';
 	return (str1);
 }
