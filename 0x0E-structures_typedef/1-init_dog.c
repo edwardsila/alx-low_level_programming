647bdcadb4cf710547f2d34dd2326fc5f256bad6
kenyanthugger <edwardsila000@gmail.com>
2023-07-17 13:12:28 -0700

    structures

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,10 +7,14 @@
  * @name: pointer to name
  * @age: age
  * @owner: string
+ * Return: nothing
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
