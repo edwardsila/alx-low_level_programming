520a2c8844739784ee9a8631ea8e596df0b00dd7
kenyanthugger <edwardsila000@gmail.com>
2023-07-17 12:56:54 -0700

    struct with typdef

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory contains all about structures and typdef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - contains elements
+ * @name: name
+ * @age: integer to age
+ * @owner: persona
+ * Description: longer description
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
