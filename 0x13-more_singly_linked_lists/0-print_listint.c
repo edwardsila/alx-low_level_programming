c47e8a3a689509815edff2e104b3f19dfc763905
kenyanthugger <edwardsila000@gmail.com>
2023-08-01 11:46:10 -0700

    ore singly linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_listint - prints all elements of a list
+ * @h: list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num_node = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num_node++;
+		h = h->next;
+	}
+
+	return (num_node);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This directory contains more linked lists...data structuresss
