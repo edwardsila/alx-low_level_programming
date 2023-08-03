a97b3169bb6efefe252043027e7f34687baea303
kenyanthugger <edwardsila000@gmail.com>
2023-08-03 11:57:14 -0700

    Bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,12 @@
 #ifndef _MAIN_H
 #define _MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 #endif
