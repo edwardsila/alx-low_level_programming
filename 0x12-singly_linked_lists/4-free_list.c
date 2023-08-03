2dcbb2831a873db013cd58b03b458a48c5a336e5
kenyanthugger <edwardsila000@gmail.com>
2023-08-03 12:31:45 -0700

    singly linked lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *free_list - frees a list_t list.
+ *@head: linked list
+ *Return: 0 on success
+ */
+void free_list(list_t *head)
+{
+	list_t *ptr;
+
+	while (head != NULL)
+	{
+		ptr = head->next;
+		free(head->str);
+		free(head);
+		head = ptr;
+	}
+}
