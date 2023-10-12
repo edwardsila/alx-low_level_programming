315be5ed97df9b8a3657c80e0a661506d3ac2afb
kenyanthugger <edwardsila000@gmail.com>
2023-10-12 05:33:33 -0700

    updated

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * free_dlistint - frees a doubly linked list
+ * @head: head pointer
+ * Return: nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)/*empty head return nothing*/
+		return;
+
+	while (head && head->next)
+	{
+		head = head->next;
+		free(head->prev);
+	}
+	free(head);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
+#include <stdlib.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -21,5 +22,6 @@ size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
 
 #endif
