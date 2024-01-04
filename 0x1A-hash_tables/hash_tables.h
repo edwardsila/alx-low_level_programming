8d058b04804669020e56a647202c692560786c1c
edwardsila <edwardsila000@gmail.com>
2024-01-04 02:01:21 -0800

    create hash

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,9 @@
-#ifndef _HASH_TABLES_
-#define _HASH_TABLES_
+#ifndef _HASHTABLES_H_
+#define _HASHTABLES_H_
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -29,5 +33,13 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
 #endif
